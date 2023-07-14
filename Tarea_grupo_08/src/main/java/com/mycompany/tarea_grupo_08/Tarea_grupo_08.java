9e1f0a7b54880a659ca4a6e40cf3142d8bfe531c
CesarMejia3433 <cejiariel.3433@gmail.com>
2023-07-13 20:19:35 -0600

    Commit proyecto corregido

--- Tarea_grupo_08/src/main/java/com/mycompany/tarea_grupo_08/Tarea_grupo_08.java
+++ Tarea_grupo_08/src/main/java/com/mycompany/tarea_grupo_08/Tarea_grupo_08.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.tarea_grupo_8;
+package com.mycompany.tarea_grupo_08;
 
 /**
  *
  * @author cejia
  */
-public class Tarea_grupo_8 {
+public class Tarea_grupo_08 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
